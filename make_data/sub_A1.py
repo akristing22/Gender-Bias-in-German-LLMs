afe689677efe6a268ed0114e0da8a429954228fa
akristing22 <121560192+akristing22@users.noreply.github.com>
2024-11-20 12:11:20 +0100

    make comments

--- make_data/sub_A1.py
+++ make_data/sub_A1.py
@@ -1,12 +1,13 @@
+# fill in the templates for dataset A1
+# fill the templates with names, occupations, and general gender markers
 import pandas as pd
 
 #male=0, female=1
-
 #read all relevant files (templates, and substitutions)
-A1_templates = pd.read_csv("../data/A1_templates.csv",encoding="utf-8-sig",sep=";")
-Names = pd.read_csv("../data/Names.csv",encoding="utf-8-sig",sep=";")
-Occupations = pd.read_csv("../data/Occupations.csv",encoding="utf-8-sig",sep=";")
-General = pd.read_csv("../data/General.csv",encoding="utf-8-sig",sep=";")
+A1_templates = pd.read_csv('../data/A1_templates.csv',encoding='utf-8-sig',sep=';')
+Names = pd.read_csv('../data/Names.csv',encoding='utf-8-sig',sep=';')
+Occupations = pd.read_csv('../data/Occupations.csv',encoding='utf-8-sig',sep=';')
+General = pd.read_csv('../data/General.csv',encoding='utf-8-sig',sep=';')
 
 #generated prompt and gender
 gender=[]
@@ -15,31 +16,32 @@ cont=[]
 subs=[]
 
 
+#given a row in the dataset, replace '[P]' with the given substitute
 def create(row,p,source):
-    prompt = row["Template"].replace("[P]",p)
+    prompt = row['Template'].replace('[P]',p)
     pr = prompt.split()
     pr[0]=pr[0].capitalize()
-    prompt = " ".join(pr)
+    prompt = ' '.join(pr)
 
-    gender.append(row["Gender"])
+    gender.append(row['Gender'])
     prompts.append(prompt)
-    cont.append(row["Continuation"])
+    cont.append(row['Continuation'])
     subs.append(source)
 
 
 for _,rowG in General.iterrows():
-    for _,row in A1_templates[(A1_templates["Gender"]==rowG["Gender"]) & (A1_templates["Case"]==rowG["Case"])].iterrows():
-            create(row,rowG["Person"],"General")
+    for _,row in A1_templates[(A1_templates['Gender']==rowG['Gender']) & (A1_templates['Case']==rowG['Case'])].iterrows():
+            create(row,rowG['Person'],'General')
 
 for _,rowO in Occupations.iterrows():
-    for _,row in A1_templates[(A1_templates["Gender"]==rowO["Gender"]) &
-                               (A1_templates["Case"]==rowO["Case"]) & 
-                               (A1_templates["Occupation"]==1)].iterrows():
-            create(row,rowO["Person"],"Occupation")
+    for _,row in A1_templates[(A1_templates['Gender']==rowO['Gender']) &
+                               (A1_templates['Case']==rowO['Case']) & 
+                               (A1_templates['Occupation']==1)].iterrows():
+            create(row,rowO['Person'],'Occupation')
 
 for _,rowN in Names.iterrows():
-    for _,row in A1_templates[(A1_templates["Gender"]==rowN["Gender"])].iterrows():
-            create(row,rowN["Person"],"Name")
+    for _,row in A1_templates[(A1_templates['Gender']==rowN['Gender'])].iterrows():
+            create(row,rowN['Person'],'Name')
 
-A1 = pd.DataFrame({"Prompt":prompts,"Gender":gender,"Continuation":cont,"Person":subs})
-A1.to_csv("../data/A1.csv",index=False,encoding="utf-8-sig",sep=";")
+A1 = pd.DataFrame({'Prompt':prompts,'Gender':gender,'Continuation':cont,'Person':subs})
+A1.to_csv('../data/A1.csv',index=False,encoding='utf-8-sig',sep=';')
--- make_data/sub_B1.py
+++ make_data/sub_B1.py
@@ -1,56 +1,58 @@
+# fill in the templates from dataset B1
+# fill the templates with names
+
 import pandas as pd
 
 #male=0, female=1
-
 #read all relevant files (templates, and substitutions)
-B1_templates = pd.read_csv("../data/B1_templates.csv",encoding="utf-8-sig",sep=";")
-Names = pd.read_csv("../data/Names.csv",encoding="utf-8-sig",sep=";")
-#Occupations = pd.read_excel("Occupations.xlsx")
-#General = pd.read_excel("General.xlsx")
+B1_templates = pd.read_csv('../data/B1_templates.csv',encoding='utf-8-sig',sep=';')
+Names = pd.read_csv('../data/Names.csv',encoding='utf-8-sig',sep=';')
 
-cols=["Ambiguous_Context",
-        "Disambiguating_Context"]
+cols=['Ambiguous_Context',
+        'Disambiguating_Context']
 
-new_cols = ["Question",
-        "Answer",
-        "Answer_stereo",
-        "Ambiguous_Context",
-        "Disambiguating_Context"]
+new_cols = ['Question',
+        'Answer',
+        'Answer_stereo',
+        'Ambiguous_Context',
+        'Disambiguating_Context']
 
-neg = ["Answer_negative",
-        "Question_negative",
-        "Question_negative_stereo_answer"]
+neg = ['Answer_negative',
+        'Question_negative',
+        'Question_negative_stereo_answer']
 
-non_neg = ["Answer_non_negative",
-            "Question_non_negative",
-            "Question_non_negative_stereo_answer"]
+non_neg = ['Answer_non_negative',
+            'Question_non_negative',
+            'Question_non_negative_stereo_answer']
 
-q_to_c = {"Answer_non_negative":"Answer","Answer_negative":"Answer",
-          "Question_non_negative":"Question", "Question_negative":"Question",
-          "Question_negative_stereo_answer":"Answer_stereo","Question_non_negative_stereo_answer":"Answer_stereo"}
+q_to_c = {'Answer_non_negative':'Answer','Answer_negative':'Answer',
+          'Question_non_negative':'Question', 'Question_negative':'Question',
+          'Question_negative_stereo_answer':'Answer_stereo','Question_non_negative_stereo_answer':'Answer_stereo'}
 
 
 new = dict()
 for c in new_cols:
     new[c] = []
-new["Gender1"] = []
-new["Gender2"] = []
-new["Name1"] = []
-new["Name2"] = []
+new['Gender1'] = []
+new['Gender2'] = []
+new['Name1'] = []
+new['Name2'] = []
 
 #sort names by length, so that is it not a factor for choosing an answer
-names_f = sorted(list(Names[Names["Gender"]==1]["Person"]),key=lambda x: len(x))
-names_m = sorted(list(Names[Names["Gender"]==0]["Person"]),key=lambda x: len(x))
+names_f = sorted(list(Names[Names['Gender']==1]['Person']),key=lambda x: len(x))
+names_m = sorted(list(Names[Names['Gender']==0]['Person']),key=lambda x: len(x))
 
+# replace the name slots in string with names
 def replace(n1,n2,val):
 
-    if "[NAME1]" in str(val):
-        val = val.replace("[NAME1]",n1)
-    if "[NAME2]" in str(val):
-        val = val.replace("[NAME2]",n2)
+    if '[NAME1]' in str(val):
+        val = val.replace('[NAME1]',n1)
+    if '[NAME2]' in str(val):
+        val = val.replace('[NAME2]',n2)
     return val
 
-
+# for a row, replace the name slots in each new column 
+# copy each row, use non-negative/negative framed answer once
 def sub(n1,n2,g1,g2,row):
     for n in neg:
         val = replace(n1,n2,row[n])
@@ -66,17 +68,17 @@ def sub(n1,n2,g1,g2,row):
             val = replace(n1,n2,row[c])
             new[c].append(val) 
 
-        new["Gender1"].append(g1)
-        new["Gender2"].append(g2)
-        new["Name1"].append(n1)
-        new["Name2"].append(n2)
+        new['Gender1'].append(g1)
+        new['Gender2'].append(g2)
+        new['Name1'].append(n1)
+        new['Name2'].append(n2)
         i+=1
 
+#substitute all name slots, in both possible order of names
 for f,m in zip(names_f,names_m):
     for _,row in B1_templates.iterrows():
         sub(f,m,1,0,row)
         sub(m,f,0,1,row)      
 
-
 B1 = pd.DataFrame(new)
-B1.to_csv("../data/B1.csv",index=False,encoding="utf-8-sig",sep=";")
+B1.to_csv('../data/B1.csv',index=False,encoding='utf-8-sig',sep=';')
